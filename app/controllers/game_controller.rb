ae16f95bc32ae84a72329a025c48c8a3fe54c31a
Sergey Timofeev <ug.tiser@gmail.com>
2016-06-15 12:02:12 +0300

    Remove controller and small fix

--- app/controllers/game_controller.rb
+++
@@ -1,7 +0,0 @@
-class GameController < ApplicationController
-
-  def index
-  	
-  end
-
-end
--- lib/display.rb
+++ lib/display.rb
@@ -1,6 +1,6 @@
 class Display
   def self.show(field)
     system("clear")
-    puts @ield.to_s.gsub('0', ' ').gsub('1', '▧')
+    puts field.to_s.gsub('0', ' ').gsub('1', '▧')
   end
 end
